29394389f48005b82f7e3bc280a9dc125a33b6ed
Dustin Lacewell <dlacewell@gmail.com>
2018-08-26 00:26:25 -0500

    Pin nixpkgs

--- default.nix
+++ default.nix
@@ -1,2 +1,7 @@
-with import <nixpkgs> {};
-callPackage ./release.nix {}
+with (import ((import <nixpkgs> { }).fetchFromGitHub {
+  owner = "NixOS";
+  repo = "nixpkgs";
+  rev = "0fd58ed0997009431306e9b0ad36016f8bdab73e";
+  sha256 = "00d8g17dfwkwxh45359ji7cl4lwx7c1g5w5y7pli7c57n48qqcay";
+})) { config = { }; };
+  callPackage ./release.nix {}
